3be6e126201911ba643430f9dd0d58e3c921f40f
Angel JS <s21910263@student.unklab.ac.id>
2022-07-24 19:19:33 +0800

    menambahkan form dengan conditional rendering

--- src/components/Registration2.js
+++ src/components/Registration2.js
@@ -7,17 +7,19 @@ import { useNavigate } from "react-router-dom";
 const Registration2 = () => {
   const [info, setInfo] = useState("");
   const [reason, setReason] = useState("");
-  const [have, setHave] = useState("");
+  const [value, setValue] = useState("");
+
 
   const navigate = useNavigate();
 
   const onSubmit = () => {
     console.log("info", info);
     console.log("reason", reason);
-    console.log("have", have);
     navigate("/regis3");
   };
 
+    
+    
   return (
     <div className="w-full">
       <div>
@@ -60,21 +62,26 @@ const Registration2 = () => {
                   <option>Lain-lain...</option>
                 </select>
               </form>
+              <div>
               <span className="block font-bold mb-1 pt-5 text-black after:content-['*'] after:text-red-500 after:ml-0.5">Apa pernah dengar "Less Waste Event" ?</span>
               <form className="flex flex-col">
-                <select value={have} onChange={(e) => setHave(e.target.value)} className="w-[284px] h-16 p-2 mr-4 rounded-full  text-black">
+                <select onChange={(e) => setValue(e.target.value)} value={value} className="w-[284px] h-16 p-2 mr-4 rounded-full  text-black">
                   <option> </option>
-                  <option>Tidak</option>
-                  <option>Pernah</option>
+                  <option value={"false"}>Tidak</option>
+                  <option value={"true"}>Pernah</option>
                 </select>
               </form>
+              </div>
+              {value ==="true" && (
+                <form>
+              <span className="block font-bold mb-1 pt-5 text-black after:content-['*'] after:text-red-500 after:ml-0.5">Kalo pernah menurut ngoni apa?</span>
+              <input type="text" className="w-[284px] h-16 p-2 mr-4 rounded-full  text-black" ></input>
+                </form>
+              )}
             </div>
           </div>
-
-          <div></div>
-
           <div className="flex justify-between relative  px-2 pt-10 pb-[100px] sm:pt-20 text-black">
-            <img className="px-[10px] md:px-[65px]" src={btnback} alt="#" />
+            <img className="px-[10px] md:px-[65px]" src={btnback} alt="#"  />
             <div className="px-[10px] md:px-[65px]">
               <button onClick={onSubmit} className="bg-gradient-to-b from-[#165E00] via-[#FFDD00] to-[#FF0000] text-white text-2xl font-bold rounded-full shadow-md shadow-gray-600 w-[137px] h-[60px] md:w-[137px] justify-end">
                 Next
@@ -89,5 +96,6 @@ const Registration2 = () => {
     </div>
   );
 };
+
 
 export default Registration2;
